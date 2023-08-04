0b9dc0aa8022d18c20d7738e59ac4f4abe21b6f2
harislee168 <haris.lee168@gmail.com>
2023-08-04 19:44:49 +0800

    added configuration for book

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/lovetocode/springbootlibrary/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/lovetocode/springbootlibrary/config/MyDataRestConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-    private String theAllowedOrigins = "http://localhost:3000";
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
                                                      CorsRegistry cors) {
@@ -21,6 +20,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         disableHTTPMethods(Book.class, config, theUnsupportedActions);
 
+        String theAllowedOrigins = "http://localhost:3000";
         cors.addMapping(config.getBasePath()+ "/**").allowedOrigins(theAllowedOrigins);
     }
 
