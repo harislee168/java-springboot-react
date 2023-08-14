dd041f9bee45cb6de7a390139345e3889d1f49d4
harislee168 <haris.lee168@gmail.com>
2023-08-14 14:10:32 +0800

    add find by user email to message repo

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/lovetocode/springbootlibrary/dao/MessageRepository.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/lovetocode/springbootlibrary/dao/MessageRepository.java
@@ -1,7 +1,13 @@
 package com.lovetocode.springbootlibrary.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.lovetocode.springbootlibrary.entity.Message;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    Page<Message> findByUserEmail(@RequestParam(name="user_email") String userEmail, Pageable pageable);
+
 }
