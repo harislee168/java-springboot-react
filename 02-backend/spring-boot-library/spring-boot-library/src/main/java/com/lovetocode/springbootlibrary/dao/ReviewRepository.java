2c6901a6b44ef9e415f01c8618f6e8ac94c87761
harislee168 <haris.lee168@gmail.com>
2023-08-15 20:29:47 +0800

    add the missing modifying annotation in the delete

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/lovetocode/springbootlibrary/dao/ReviewRepository.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/lovetocode/springbootlibrary/dao/ReviewRepository.java
@@ -4,6 +4,7 @@ import com.lovetocode.springbootlibrary.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +15,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Review findByUserEmailAndBookId(@RequestParam(name="user_email") String userEmail,
                                     @RequestParam(name="book_id") Long bookId);
-
+    @Modifying
     @Query(value="DELETE FROM review WHERE book_id=:book_id", nativeQuery=true)
     void deleteAllByBookId(@Param(value="book_id") Long bookId);
 }
