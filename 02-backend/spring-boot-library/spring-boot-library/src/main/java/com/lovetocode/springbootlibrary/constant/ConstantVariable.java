dbde997947842e8b42bc61e52a132e3390fcee6f
harislee168 <haris.lee168@gmail.com>
2023-08-16 11:59:20 +0800

    change the base url intp https:

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/lovetocode/springbootlibrary/constant/ConstantVariable.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/lovetocode/springbootlibrary/constant/ConstantVariable.java
@@ -2,5 +2,5 @@ package com.lovetocode.springbootlibrary.constant;
 
 public class ConstantVariable {
 
-    public static final String BASE_URL = "http://localhost:3000";
+    public static final String BASE_URL = "https://localhost:3000";
 }
