3148cd33fd61c63fc8da2b06a11de36e09b2363b
harislee168 <haris.lee168@gmail.com>
2023-08-19 11:45:21 +0800

    remove the unneccessary import

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/lovetocode/springbootlibrary/service/BookService.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/lovetocode/springbootlibrary/service/BookService.java
@@ -9,7 +9,6 @@ import com.lovetocode.springbootlibrary.entity.Checkout;
 import com.lovetocode.springbootlibrary.entity.History;
 import com.lovetocode.springbootlibrary.entity.Payment;
 import com.lovetocode.springbootlibrary.responsemodel.ShelfCurrentLoansResponse;
-import org.hibernate.annotations.Check;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
